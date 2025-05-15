47dc7fb532b6b70938d3da4e68a804dba2d8f3f0
Andrei-Octavian Buha <buha2595@gmail.com>
2025-05-15 19:29:53 +0200

    i work on skills

--- src/app/main-content/skills/skills.component.ts
+++ src/app/main-content/skills/skills.component.ts
@@ -9,4 +9,25 @@ import { CommonModule } from '@angular/common';
 })
 export class SkillsComponent {
 
+  isVisible = true;
+
+  toggle() {
+    this.isVisible = !this.isVisible; // Inversează valoarea
+  }
+
+  images:Array <string> =['html','css','js','ts','angular','firebase','git','api','scrum','materialDesign'];
+
+  objImages:Array<{src: string, alt: string}> = [
+    {src:'html', alt:'HTML'},
+    {src:'css', alt:'CSS'},
+    {src:'js', alt:'JavaScript'},
+    {src:'ts', alt:'Typescript'},
+    {src:'angular', alt:'Angular'},
+    {src:'firebase', alt:'Firebase'},
+    {src:'git', alt:'GIT'},
+    {src:'api', alt:'API'},
+    {src:'scrum', alt:'Scrum'},
+    {src:'materialDesign', alt:'Material Design'},
+  ];
+
 }
