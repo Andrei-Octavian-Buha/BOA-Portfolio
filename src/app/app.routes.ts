e3ab25be940e65ace5689b2ae5b84878a4807cf4
Octavian <buha2595@gmail.com>
2025-06-24 21:21:41 +0200

    i work on navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,8 @@ import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 
 export const routes: Routes = [
-    { path: '', component: MainContentComponent },
+    {   path: '', 
+        component: MainContentComponent,
+        title: 'Home - BOA Portfolio',
+        },
 ];
--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogoComponent } from '../logo/logo.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [CommonModule , LogoComponent],
+  imports: [CommonModule , LogoComponent, RouterLink],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
